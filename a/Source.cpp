2fd40f4e4bc6168e0acae0a18a0c75fe4fe87d6f
Paul A. Fischer <pfischer@carthage.edu>
2017-04-25 14:49:52 -0500

    Initial Commit
    This is optional, Summary is Necessary

---
+++ a/Source.cpp
@@ -0,0 +1,16 @@
+//Paul A. Fischer
+//Perry Kivolowitz
+//Computer Science II
+//Github Porting test
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hey!" << endl;
+
+	system ("pause");
+	return 0;
+}
